a12783f0e2bd7ce465318bf9e7085505606f3ff6
DedSecCyber <dedseccyberteam@gmail.com>
2020-10-30 18:46:01 +0700

    argv update

--- urlfuzzer.py
+++ urlfuzzer.py
@@ -1,11 +1,19 @@
 import requests
+import sys
 
-host = "https://www.google.com"
-f = open("wordlist.txt", "r", encoding="utf-8")
+def fuzzing(files, host):
+	f = open(files, "r", encoding="utf-8")
 
-for i in f:
-	r = requests.get(host+i)
-	if r.status_code == 200:
-		print("Found 200 ", host+i)
+	for i in f:
+		r = requests.get(host+i)
+		if r.status_code == 200:
+			print("Found 200 ", host+i)
+		else:
+			pass
+
+class main():
+	if len(sys.argv) != 3:
+		print("Usage : ", sys.argv[0], " <web target> <directory list>")
+		sys.exit()
 	else:
-		pass
+		fuzzing(sys.argv[1], sys.argv[2])
